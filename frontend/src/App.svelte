cea8223d0aba2247b0867a05371e5f5b7b7842f9
Alfex4936 <ikr@kakao.com>
2022-06-24 14:10:20 +0900

    Weather reloading
    every hour or clicking icon

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,297 +1,343 @@
 <script context="module">
-  export let object = {
-    number_of_notice: 7,
-  };
-
-  
+    export let object = {
+        number_of_notice: 7,
+    };
 </script>
 
 <script lang="ts">
-  // import logo from "./assets/images/ajou-logo.png";
-  
-  // import {main} from '../wailsjs/go/models/models'
+    // import logo from "./assets/images/ajou-logo.png";
 
-  // var notices: Array<main.Notice>;
-  // let notice = new main.Notice()
-  // let notices: Array<main.Notice> = [];
+    // import {main} from '../wailsjs/go/models/models'
 
-  import NoticeList from "./components/NoticeList.svelte";
+    // var notices: Array<main.Notice>;
+    // let notice = new main.Notice()
+    // let notices: Array<main.Notice> = [];
 
-  import { GetWeather, Parse } from "../wailsjs/go/main/App";
+    import NoticeList from "./components/NoticeList.svelte";
 
-  let weather_promise = GetWeather();
-  let notices_promise = Parse("", 7);
-  let number_of_notice = 7;
-  let last_menu = 1;
+    import { GetWeather, Parse } from "../wailsjs/go/main/App";
 
-  const ajou_link = "https://www.ajou.ac.kr/kr/ajou/notice.do";
+    let weather_promise = GetWeather();
+    let notices_promise = Parse("", 7);
+    let number_of_notice = 7;
+    let last_menu = 1;
 
-  function updateNotice(menu = 1) {
-      let category = "";
+    // let weather_updater: ReturnType<typeof setTimeout>
+    setTimeout(() => {
+        weather_promise = GetWeather();
+    }, 1000 * Math.floor(Math.random() * (610 - 590 + 1) + 590));
 
-      switch (menu) {
-          case 2: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  1 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 3: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  2 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 4: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  3 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 5: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  4 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 6: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  5 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 7: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  6 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 8: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  7 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 9: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  8 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 10: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  166 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 11: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  167 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          case 12: {
-              category =
-                  ajou_link +
-                  "?mode=list&srCategoryId=" +
-                  168 +
-                  "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
-                  number_of_notice;
-              break;
-          }
-          default: {
-              category = "";
-              break;
-          }
-      }
+    const ajou_link = "https://www.ajou.ac.kr/kr/ajou/notice.do";
 
-      notices_promise = Parse(category, number_of_notice);
-  }
+    function updateNotice(menu = 1) {
+        let category = "";
+
+        switch (menu) {
+            case 2: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    1 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 3: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    2 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 4: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    3 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 5: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    4 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 6: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    5 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 7: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    6 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 8: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    7 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 9: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    8 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 10: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    166 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 11: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    167 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            case 12: {
+                category =
+                    ajou_link +
+                    "?mode=list&srCategoryId=" +
+                    168 +
+                    "&srSearchKey=&srSearchVal=&article.offset=0&articleLimit=" +
+                    number_of_notice;
+                break;
+            }
+            default: {
+                category = "";
+                break;
+            }
+        }
+
+        notices_promise = Parse(category, number_of_notice);
+    }
 </script>
 
 <main>
-  <ul id="menu">
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 1; updateNotice(1)}}>전체 공지</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 2; updateNotice(2)}}>학사</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 3; updateNotice(3)}}>비교과</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 4; updateNotice(4)}}>장학</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 5; updateNotice(5)}}>학술</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 6; updateNotice(6)}}>입학</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 7; updateNotice(7)}}>취업</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 8; updateNotice(8)}}>사무</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 9; updateNotice(9)}}>기타</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 10; updateNotice(10)}}>행사</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 11; updateNotice(11)}}>파란학기제</a
-        >
-    </li>
-    |
-    <li>
-        <a
-            class="category"
-            href="/"
-            on:click|preventDefault={() => {last_menu = 12; updateNotice(12)}}>학사일정</a
-        >
-    </li>
-</ul>
+    <ul id="menu">
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 1;
+                    updateNotice(1);
+                }}>전체 공지</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 2;
+                    updateNotice(2);
+                }}>학사</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 3;
+                    updateNotice(3);
+                }}>비교과</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 4;
+                    updateNotice(4);
+                }}>장학</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 5;
+                    updateNotice(5);
+                }}>학술</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 6;
+                    updateNotice(6);
+                }}>입학</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 7;
+                    updateNotice(7);
+                }}>취업</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 8;
+                    updateNotice(8);
+                }}>사무</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 9;
+                    updateNotice(9);
+                }}>기타</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 10;
+                    updateNotice(10);
+                }}>행사</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 11;
+                    updateNotice(11);
+                }}>파란학기제</a
+            >
+        </li>
+        |
+        <li>
+            <a
+                class="category"
+                href="/"
+                on:click|preventDefault={() => {
+                    last_menu = 12;
+                    updateNotice(12);
+                }}>학사일정</a
+            >
+        </li>
+    </ul>
 
-  <!-- <img alt="ajou logo" id="logo" src={logo} /> -->
-  {#await weather_promise}
-    <p>🌞 날씨 불러오는 중...</p>
-  {:then weather}
-    <img alt="weather" id="weather" src={weather.icon} />
-    <p>
-      아주대 현재 날씨 {weather.current_temp} ({weather.current_stat}), 최저
-      기온은 {weather.min_temp}, 최고 기온은 {weather.max_temp}<br />
-      낮 강수량은 {weather.rain_day}, 밤 강수량은 {weather.rain_night}<br />
-      내일 낮 강수량은 {weather.tmrw_rain_day}, 밤 감수량은 {weather.tmrw_rain_night}
-    </p>
-  {:catch error}
-    <p style="color:red">{error.message}</p>
-  {/await}
+    <!-- <img alt="ajou logo" id="logo" src={logo} /> -->
+    {#await weather_promise}
+        <p>🌞 날씨 불러오는 중...</p>
+    {:then weather}
+        <img
+            on:click={() => (weather_promise = GetWeather())}
+            title="눌러서 새로고침 하세요"
+            alt="weather"
+            id="weather"
+            src={weather.icon}
+        />
+        <p>
+            아주대 현재 날씨 {weather.current_temp} ({weather.current_stat}),
+            최저 기온은 {weather.min_temp}, 최고 기온은 {weather.max_temp}<br />
+            낮 강수량은 {weather.rain_day}, 밤 강수량은 {weather.rain_night}<br
+            />
+            내일 낮 강수량은 {weather.tmrw_rain_day}, 밤 감수량은 {weather.tmrw_rain_night}
+        </p>
+    {:catch error}
+        <p style="color:red">{error.message}</p>
+    {/await}
 
-  <p style="display:inline">공지 갯수:</p>
-  <input
-    type="number"
-    min="1"
-    max="50"
-    style="text-align:center"
-    name="notice"
-    bind:value={number_of_notice}
-    placeholder="공지 갯수"
-  />
-  <button on:click={() => updateNotice(last_menu)}>공지 불러오기</button>
+    <p style="display:inline">공지 갯수:</p>
+    <input
+        type="number"
+        min="1"
+        max="50"
+        style="text-align:center"
+        name="notice"
+        bind:value={number_of_notice}
+        placeholder="공지 갯수"
+    />
+    <button on:click={() => updateNotice(last_menu)}>공지 불러오기</button>
 
-  {#await notices_promise}
-    <p>💌 공지 불러오는 중...</p>
-  {:then notices}
-    <NoticeList {notices} />
-  {:catch _}
-    <p>공지 불러오는 중 에러 발생!</p>
-  {/await}
+    {#await notices_promise}
+        <p>💌 공지 불러오는 중...</p>
+    {:then notices}
+        <NoticeList {notices} />
+    {:catch _}
+        <p>공지 불러오는 중 에러 발생!</p>
+    {/await}
 </main>
 
 <style>
-  #logo {
-    display: block;
-    width: 200px;
-    height: 182px;
-    margin: auto;
-    /* padding: 10% 0 0; */
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: 100% 100%;
-    background-origin: content-box;
-  }
+    #logo {
+        display: block;
+        width: 200px;
+        height: 182px;
+        margin: auto;
+        /* padding: 10% 0 0; */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        background-origin: content-box;
+    }
 
-  #weather {
-    width: 10%;
-  }
+    #weather {
+        width: 10%;
+    }
 
-  ul#menu li {
+    ul#menu li {
         display: inline;
     }
 
