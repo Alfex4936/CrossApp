6ac703f82934403fb55c687a90f5b4fd3a1bbe9e
Alfex4936 <ikr@kakao.com>
2022-07-06 16:21:05 +0900

    signed

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 wails build -upx -platform=windows
 wails build -upx
 chmod +x build/bin/ajou
+
+/home/seok/Limelighter/Limelighter -I build/bin/ajou.exe -O build/bin/signed_ajou.exe -Domain www.naver.com
+rm build/bin/ajou.exe
+mv build/bin/signed_ajou.exe build/bin/ajou.exe
