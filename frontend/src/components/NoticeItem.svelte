20ac00d5f5baf8db12c9178b854d4deee867dea7
Alfex4936 <ikr@kakao.com>
2022-07-06 12:39:45 +0900

    Update binary

--- frontend/src/components/NoticeItem.svelte
+++ frontend/src/components/NoticeItem.svelte
@@ -1,10 +1,17 @@
 <script>
     export let notice;
+
+    import { BrowserOpenURL } from "../../wailsjs/runtime";
 </script>
 
 <li>
-    <a target="_blank" rel="noopener" href={notice.link}>{notice.title}</a><br
-    />({notice.category}) {notice.writer} - {notice.date}
+    <!-- <a target="_blank" rel="noopener" href={notice.link}>{notice.title}</a><br
+    />({notice.category}) {notice.writer} - {notice.date} -->
+
+    <p style="font-weight:bold" on:click={() => BrowserOpenURL(notice.link)}>
+        {notice.title}
+    </p>
+    ({notice.category}) {notice.writer} - {notice.date}
 </li>
 
 <style>
